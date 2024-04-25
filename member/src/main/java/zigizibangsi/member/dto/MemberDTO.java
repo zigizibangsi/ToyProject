636fe6697294384cdc225dcff4c71bf7f34c4718
zigizibangsi <zigizibangsi@gmail.com>
2024-04-25 18:00:44 +0900

    member

---
+++ member/src/main/java/zigizibangsi/member/dto/MemberDTO.java
@@ -0,0 +1,27 @@
+package zigizibangsi.member.dto;
+
+import lombok.*;
+import zigizibangsi.member.entity.MemberEntity;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class MemberDTO {
+    private Long id;
+    private String memberEmail;
+    private String memberPassword;
+    private String memberName;
+
+    public static MemberDTO toMemberDTO(MemberEntity memberEntity) {
+        MemberDTO memberDTO = new MemberDTO();
+        memberDTO.setId(memberEntity.getId());
+        memberDTO.setMemberEmail(memberEntity.getMemberEmail());
+        memberDTO.setMemberPassword(memberEntity.getMemberPassword());
+        memberDTO.setMemberName(memberEntity.getMemberName());
+        return memberDTO;
+    }
+
+}
---
+++ member/src/main/java/zigizibangsi/member/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package zigizibangsi.member.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import zigizibangsi.member.entity.MemberEntity;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+    // 이메일로 회원 정보 조회 (select * from member_table where member_email=?)
+    Optional<MemberEntity> findByMemberEmail(String memberEmail);
+}
